1db23f5eacea85d47316d3003d3e0f2d98626164
EdwinSch <edwinschutjes@gmail.com>
2023-07-26 15:15:27 +0200

    style: advert component

--- src/components/Advert.jsx
+++ src/components/Advert.jsx
@@ -1,7 +1,17 @@
+import Logo from "./Logo";
+
 const Advert = () => {
   return (
     <section className="advert-container">
-      <h2>logo</h2>
+      <Logo />
+      <h1>
+        <span>we're</span>coming <br /> soon
+      </h1>
+      <p>
+        Hello fellow shoppers! We're currently building our new fashion store.
+        Add your email below to stay up-to-date with announcements and our
+        launch deals.
+      </p>
     </section>
   );
 };
