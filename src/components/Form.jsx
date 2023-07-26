90ec702787f95d172e253464f06e97cf17bdc272
EdwinSch <edwinschutjes@gmail.com>
2023-07-26 16:15:36 +0200

    add: form functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,46 @@
+import { useRef, useState } from "react";
 import { BsChevronRight } from "react-icons/bs";
 
 const Form = () => {
+  const [validation, setValidation] = useState(true);
+
+  // useRef hook
+  const input = useRef(null);
+
+  // Handle submit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (
+      !input.current.value.includes("@") ||
+      !input.current.value.includes(".")
+    ) {
+      setValidation(false);
+      console.log("not validated");
+      return;
+    }
+
+    console.log(input.current.value);
+    input.current.value = "";
+    setValidation(true);
+  };
+
+  // Component render
   return (
-    <form>
-      <input type="email" name="email" placeholder="Email Address" />
+    <form onSubmit={handleSubmit} noValidate>
+      <input
+        className={validation ? "email" : "email error-state"}
+        type="email"
+        name="email"
+        placeholder="Email Address"
+        ref={input}
+      />
       <button type="submit">
         <BsChevronRight />
       </button>
+      {validation || (
+        <p className="error-message">Please provide a valid email</p>
+      )}
     </form>
   );
 };
